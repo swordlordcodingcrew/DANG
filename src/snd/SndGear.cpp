2da3e2b1a1cbb868f5520642d0fbdb61321493bd
LordFilu <lordfilu@swordlord.com>
2021-02-04 21:35:52 +0100

    sfx

--- src/snd/SndGear.cpp
+++ src/snd/SndGear.cpp
@@ -6,12 +6,14 @@
 
 #include <iostream>
 #include "SndGear.hpp"
+#include "wave.h"
 
 namespace dang
 {
     bool SndGear::mod_set = false;
-    uint8_t SndGear::chan = 1;
-    static pocketmod_context context;
+    uint8_t SndGear::chan = 0;
+    pocketmod_context SndGear::context;
+    std::vector<SndGear::sfx_struct> SndGear::_sfx_container(7, {nullptr, 0, 0, 0, 0});
 
     void SndGear::setMod(const uint8_t *mod, const uint32_t len)
     {
@@ -42,7 +44,7 @@ namespace dang
      * @param wave_buffer
      * @return loop count of mod
      */
-    uint32_t SndGear::fillWaveBufferWithMod(int16_t *wave_buffer)
+    uint32_t SndGear::fillWaveBufferWithMod(int16_t wave_buffer[64])
     {
         if (mod_set)
         {
@@ -66,6 +68,63 @@ namespace dang
         }
 
         return 0;
+    }
+
+    uint32_t SndGear::fillWaveBufferWithSfx(int16_t wave_buffer[64], uint8_t ch)
+    {
+//        sfx_struct& sfxs = SndGear::_sfx_container[ch-1];
+        sfx_struct& sfxs = SndGear::_sfx_container.at(ch-1);
+
+        // Copy 64 bytes to the channel audio buffer
+        for (int x = 0; x < 64; x++)
+        {
+            // If current sample position is greater than the sample length, fill the rest of the buffer with zeros.
+            int16_t val = ((int16_t*)sfxs.sfx)[sfxs.pos];
+            int16_t sample = sfxs.pos < sfxs.len ? (sfxs.sfx[sfxs.pos] << 8) - 0x7f00 : 0;
+//            wave_buffer[x] = sfxs.pos < sfxs.len ? (sfxs.sfx[sfxs.pos] << 8) - 0x7f00 : 0;
+            wave_buffer[x] = sfxs.pos < sfxs.len ? val : 0;
+
+            sfxs.pos++;
+        }
+
+        if (sfxs.pos >= sfxs.len)
+        {
+            sfxs.loops++;
+        }
+
+        return sfxs.loops;
+    }
+
+    int16_t SndGear::setSfx(const uint8_t *sfx, const uint32_t len)
+    {
+        const wave_header* wh = (wave_header*)sfx;
+
+        if (wh->format_type != 1)
+        {
+            std::cout << "unsupported format ( format type " << wh->format_type << ")" << std::endl;
+            return -1;
+        }
+
+        if (wh->channels != 1)
+        {
+            std::cout << "wave is stereo, but should be mono" << std::endl;
+            return -1;
+        }
+
+        if (wh->sample_rate != 22050)
+        {
+            std::cout << "sample rate is not 22050" << std::endl;
+            return -1;
+        }
+
+        uint8_t ch = getSfxChan();
+        SndGear::_sfx_container.at(ch-1).chan = ch;
+        SndGear::_sfx_container.at(ch-1).len = wh->data_size / (wh->bits_per_sample / 8) + sizeof(wave_header);
+        SndGear::_sfx_container.at(ch-1).sfx = sfx;
+        SndGear::_sfx_container.at(ch-1).pos = sizeof(wave_header);
+        SndGear::_sfx_container.at(ch-1).loops = 0;
+
+        return ch;
     }
 
 }
--- src/snd/SndGear.hpp
+++ src/snd/SndGear.hpp
@@ -5,6 +5,7 @@
 
 
 #include <cstdint>
+#include <vector>
 
 struct pocketmod_context;
 
@@ -14,17 +15,30 @@ namespace dang
     {
     public:
         static void setMod(const uint8_t* mod, const uint32_t len);
-        static uint32_t fillWaveBufferWithMod(int16_t* wave_buffer);
+        static int16_t setSfx(const uint8_t* sfx, const uint32_t len);
+        static uint32_t fillWaveBufferWithMod(int16_t wave_buffer[64]);
+        static uint32_t fillWaveBufferWithSfx(int16_t wave_buffer[64], uint8_t ch);
 
         static uint8_t getMusicChan() { return 0; }
-        static uint8_t getSfxChan() { return ++chan % 15 + 1; }
 
         static bool mod_set;
+
+        struct sfx_struct
+        {
+            const uint8_t* sfx;
+            uint32_t len;
+            uint8_t chan;
+            uint32_t pos;
+            uint32_t loops;
+        };
 
     protected:
         static uint8_t chan;
         static float clip(float value);
-//        static pocketmod_context context;
+        static pocketmod_context context;
+        static std::vector<sfx_struct> _sfx_container;
+
+        static uint8_t getSfxChan() { return chan++ % 7 + 1; }
     };
 
 }
