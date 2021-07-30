41ac316882bca7a4400db97977083801c8764c69
LordFilu <lordfilu@swordlord.com>
2021-07-30 06:47:47 +0200

    code cleanup

--- src/DangFwdDecl.h
+++ src/DangFwdDecl.h
@@ -2,6 +2,7 @@
 // This file is part of the DANG game framework
 
 #pragma once
+
 #include <memory>
 
 namespace dang
@@ -29,16 +30,25 @@ namespace dang
     using spTweenable = std::shared_ptr<Tweenable>;
     class TwAnim;
     using spTwAnim = std::shared_ptr<TwAnim>;
+    class TwNull;
+    using spTwNull = std::shared_ptr<TwNull>;
+    class TwSequence;
+    using spTwSequence = std::shared_ptr<TwSequence>;
+
 
     /** remaining classes */
     class CollisionSprite;
     using spCollisionSprite = std::shared_ptr<CollisionSprite>;
+    class CollisionSpriteLayer;
+    using spCollisionSpriteLayer = std::shared_ptr<CollisionSpriteLayer>;
+    class Gear;
     class Imagesheet;
     using spImagesheet = std::shared_ptr<Imagesheet>;
     class Sprite;
     using spSprite = std::shared_ptr<Sprite>;
     class Layer;
     using spLayer = std::shared_ptr<Layer>;
+    class Event;
 
     /** tmx extruder classes */
     struct tmx_spriteobject;
