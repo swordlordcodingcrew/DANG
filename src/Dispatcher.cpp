8ef548e49ef00024f31157d71b2bb01fb24fe8be
LordEidi <lordeidi@swordlord.com>
2021-08-26 17:15:11 +0200

    A bit of spring cleaning (and removed old forward list code)

--- src/Dispatcher.cpp
+++ src/Dispatcher.cpp
@@ -1,4 +1,4 @@
-// (c) 2019-20 by SwordLord - the coding crew
+// (c) 2019-21 by SwordLord - the coding crew
 // This file is part of the DANG game framework
 
 #include <algorithm>
@@ -7,23 +7,6 @@
 
 namespace dang
 {
-
-/*    void Dispatcher::publishEvent(Event &e)
-    {
-        for (std::pair<const unsigned int, _subscriber_wrapper>& pair : _subscribers)
-        {
-            if ((pair.second.filt & e._filter) > 0)
-            {
-                pair.second.fn(e);
-
-                if (e._consumed)
-                {
-                    break; // for-loop
-                }
-            }
-        }
-    }
-*/
     void Dispatcher::removeSubscriber(uint32_t index)
     {
         _subscribers.erase(index);
@@ -66,9 +49,6 @@ namespace dang
 
     void Dispatcher::queueEvent(std::unique_ptr<Event> e)
     {
-        // remove me after push
-        //_event_list_filler.push_front(std::move(e));
-
         _events.push(std::move(e));
     }
 
@@ -86,36 +66,5 @@ namespace dang
                 }
             }
         }
-
-        // remove me after push
-
-        /*
-        _event_list.splice_after(_event_list.cbefore_begin(), _event_list_filler);
-
-        for (const auto& e : _event_list)
-        {
-            for (std::pair<const uint32_t , _subscriber_wrapper>& pair : _subscribers)
-            {
-                if ((pair.second.filt & e->_filter) > 0)
-                {
-                    pair.second.fn(*e);
-
-                    if (e->_consumed)
-                    {
-                        break; // for-loop
-                    }
-                }
-            }
-
-        }
-
-        if (!_event_list.empty())
-        {
-            //_event_list.remove_if([](const std::unique_ptr<Event>& e) { return e->_consumed; });
-
-            _event_list.clear();
-        }
-         */
     }
-
 }
