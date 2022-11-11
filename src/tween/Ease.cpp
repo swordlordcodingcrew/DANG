69fd7d0b9fb3d5bcba97413fe17eb7a5f4f40442
LordFilu <lordfilu@swordlord.com>
2022-11-11 09:36:34 +0100

    resolved warning: operation on may be undefined [-Wsequence-point]

--- src/tween/Ease.cpp
+++ src/tween/Ease.cpp
@@ -7,5 +7,24 @@
 
 namespace dang
 {
-    // empty for now..
+    float Ease::OutCubic(const float x)
+    {
+        float f = x - 1;
+        return 1 + f * f * f;
+
+    }
+
+    float Ease::InOutCubic(const float x)
+    {
+        if (x < 0.5f)
+        {
+            return 4 * x * x * x;
+        }
+        else
+        {
+            float f = ((2 * x) - 2);
+            return 0.5f * f * f * f + 1;
+        }
+//        return x < 0.5f ? 4 * x * x * x : 1 + (--x) * (2 * (--x)) * (2 * x);
+    }
 }
